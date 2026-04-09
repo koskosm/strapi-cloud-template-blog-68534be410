63a78c434453651799c6307e664fb7f2da11d627
Frank Lam <franklam@oursky.com>
2026-04-09 17:15:55 +0800

    Update package-lock.json and package.json to reflect dependency version changes, including downgrades for @casl/ability, @strapi/admin, and others, along with the addition of overrides for @strapi/admin.

--- src/admin/app.js
+++ src/admin/app.js
@@ -17,10 +17,7 @@ export default {
         defaultMessage: 'Comma-separated slugs with autocomplete per token.',
       },
       components: {
-        Input: async () =>
-          import('./components/CreditCardSlugsInput').then((module) => ({
-            default: module.default,
-          })),
+        Input: () => import('./components/CreditCardSlugsInput'),
       },
     });
   },
