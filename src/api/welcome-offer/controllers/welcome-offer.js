969a4585e50d8c6585dd5478d3910f0071070207
Frank Lam <franklam@oursky.com>
2026-04-21 15:08:04 +0800

    Remove welcome-offer API components including schema, controller, router, and service to streamline the codebase.

--- src/api/welcome-offer/controllers/welcome-offer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * welcome-offer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::welcome-offer.welcome-offer');
--- src/api/welcome-offer/services/welcome-offer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * welcome-offer service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::welcome-offer.welcome-offer');
