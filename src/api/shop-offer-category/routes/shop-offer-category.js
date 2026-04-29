96baa310b6743f7f8bd604fe9ab997d5c0f6af39
Frank Lam <franklam@oursky.com>
2026-04-29 17:08:19 +0800

    feat: introduce shop-offer-category API with schema, controller, router, and service for enhanced shop offer management

---
+++ src/api/shop-offer-category/routes/shop-offer-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-offer-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shop-offer-category.shop-offer-category');
---
+++ src/api/shop-offer-category/services/shop-offer-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-offer-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shop-offer-category.shop-offer-category');
